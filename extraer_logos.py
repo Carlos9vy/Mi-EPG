c2982463f2fb5fa3d2de7567ae085a3e756a2770
Carlos9VY <65052443+Carlos9vy@users.noreply.github.com>
2026-05-29 20:17:37 -0500

    Add new XML source URL for EPG extraction

--- extraer_logos.py
+++ extraer_logos.py
@@ -16,6 +16,7 @@ EPG_SOURCES = [
     "https://iptv-epg.org/files/epg-hn.xml",
     "https://iptv-epg.org/files/epg-py.xml",
     "https://iptv-epg.org/files/epg-pa.xml",
+    "https://github.com/Carlos9vy/mi-laboratorio-epg/raw/refs/heads/main/guia_laboratorio.xml",
     "https://www.open-epg.com/generate/aYzuzNSenh.xml.gz" 
 ]
 
